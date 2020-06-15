aa833e0c6a20c19036a24ba1f2b3558ea63005f5
nk.warjurkar <nk.warjurkar@gmail.com>
2020-06-15 11:47:06 +0530

    Initial Commit

---
+++ SukshiTaskFramework/SukshiTaskFramework.h
@@ -0,0 +1,19 @@
+//
+//  SukshiTaskFramework.h
+//  SukshiTaskFramework
+//
+//  Created by Nitu Warjurkar on 15/06/20.
+//  Copyright © 2020 Nitu Warjurkar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SukshiTaskFramework.
+FOUNDATION_EXPORT double SukshiTaskFrameworkVersionNumber;
+
+//! Project version string for SukshiTaskFramework.
+FOUNDATION_EXPORT const unsigned char SukshiTaskFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SukshiTaskFramework/PublicHeader.h>
+
+
